356167a9afea50aeb1f14603e6f5e336621683d9
Green-Pentagon <115361212+Green-Pentagon@users.noreply.github.com>
2025-02-09 15:53:22 +0000

    basicEditorScripting

--- Assets/Assets/Scripts/Generator/Configure.cs
+++ Assets/Assets/Scripts/Generator/Configure.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class Configure : MonoBehaviour
 {
+
+    //----------------------------------
     public const float UNIT_SIZE = 1.0f;
 
+    //IF YOU CHANGE THESE VALUES, ALSO CHANGE THE VALUES OFR THE PUBLIC VARIABLES BELOW ACCORDINGLY.
+    private int[] roomWidthRange = { 1, 100 };
+    private int[] roomHeightRange = { 1, 100 };
+    //private int[] roomCountRange = { 1, 6 };
+    //----------------------------------
+
+
+    //IN-EDITOR SLIDERS
+    //----------------------------------
+    //room count sliders
+    [Range(1, 6)]
+    //[Min(1), Tooltip("WARNING: High room counts may impact performance!")]
     public int numRooms = 1;
-    public int[] roomWidthRange = {1,5};
-    public int[] roomHeightRange = {1,5};
-    
-    
+
+    //room width sliders
+    [Range(1, 100)]
+    public int minimumWidth;
+    [Range(1, 100)]
+    public int maximumWidth;
+
+    //room height sliders
+    [Range(1, 100)]
+    public int minimumHeight;
+    [Range(1, 100)]
+    public int maximumHeight;
+    //----------------------------------
+
+    //IN-EDITOR SLIDER VALIDATION
+    //----------------------------------
+    [ExecuteInEditMode]
+    void OnValidate()
+    {
+
+        if (minimumWidth > maximumWidth && maximumWidth != roomWidthRange[1])
+        {
+            minimumWidth++;
+        }
+
+        if (minimumHeight > maximumHeight && maximumHeight != roomHeightRange[1])
+        {
+            maximumHeight++;
+        }
+    }
+    //----------------------------------
+
     private List<Object> rooms;
     void Start()
     {
