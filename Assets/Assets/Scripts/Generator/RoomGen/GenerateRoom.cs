fbbab041fe25fa073fecfc8ee3edc2f1e4cb512e
Green-Pentagon <115361212+Green-Pentagon@users.noreply.github.com>
2025-02-16 13:02:59 +0000

    non-wall room generator
    ready for separation stage of the algorithm.

--- Assets/Assets/Scripts/Generator/RoomGen/GenerateRoom.cs
+++ Assets/Assets/Scripts/Generator/RoomGen/GenerateRoom.cs
@@ -24,14 +24,27 @@ public class GenerateRoom : MonoBehaviour
     public Object CreateRoom(Sprite FloorTile, float unit, int width, int height)
     {
         GameObject room = new GameObject();
+        room.name = "Room";
+
+        //Seperation Logic Collider
+        room.AddComponent<BoxCollider2D>();
+        room.GetComponent<BoxCollider2D>().size = new Vector2(width*unit, height*unit);
+        room.GetComponent<BoxCollider2D>().offset = new Vector2(unit, unit);
+        room.AddComponent<Rigidbody2D>();
+        room.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+        room.GetComponent<Rigidbody2D>().freezeRotation = true;
+
+
 
         GameObject floorTile = new GameObject();
+        floorTile.name = "FloorTile";
         floorTile.AddComponent<SpriteRenderer>();
         floorTile.GetComponent<SpriteRenderer>().sprite = FloorTile;
-        floorTile.GetComponent<Transform>().lossyScale.Set(1.0f * unit, 1.0f * unit, 1.0f);
+        floorTile.GetComponent<Transform>().localScale = new Vector3(1.0f * unit, 1.0f * unit, 1.0f);
 
         GameObject tempFloor;
         
+        //create tiles for the room
         for (int x = 0; x < width;x++)
         {
             for (int y = 0; y < height; y++)
